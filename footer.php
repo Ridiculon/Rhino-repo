935067f21883ed14ef94956988ea49b34d36d1f0
vrxacs <vrx@spark>
2012-08-18 16:41:47 -0500

    Issue #6

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <div class="footer">
-	
+	<a href="./account.php">Account</a>&nbsp; &nbsp; <a href="./contests.php">Contests</a>&nbsp; &nbsp; <a href="./about.php">About</a>  
+	</br>Contact us on <a href="http://www.facebook.com/pages/RhinoLaunch/207060262663763">Facebook</a>!
+	</br>Copyright &#9426; 2012 <font style="color: #ffffff">Rhinolaunch</font>
+	</br>All rights reserved.
+	</br></br>
+	Design by <font style="color: #ffffff">Shakstyle</font>
 </div>
 		
 
